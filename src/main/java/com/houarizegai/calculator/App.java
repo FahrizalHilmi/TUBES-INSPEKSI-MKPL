e7fc3318a833e187f27ed46a5242c581d8093426
FahrizalHilmi <74576129+FahrizalHilmi@users.noreply.github.com>
2023-06-12 16:56:02 +0700

    Update App.java

--- src/main/java/com/houarizegai/calculator/App.java
+++ src/main/java/com/houarizegai/calculator/App.java
@@ -6,5 +6,24 @@ public class App {
 
     public static void main(String[] args) {
         new CalculatorUI();
+        
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
+        System.out.print("Calculator");
     }
 }
